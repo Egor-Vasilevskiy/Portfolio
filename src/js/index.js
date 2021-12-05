67d746a0c4818080b3e5c670c65b04baed610bb1
Egor-Vasilevskiy <70340503+Smailent@users.noreply.github.com>
2021-12-05 20:42:27 +0300

    Add multilingualism

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,39 @@ window.onscroll = function() {
     }
 	prevScrollPos = currentScrollPos;
 };
+
+//Мультиязычность
+
+const select = document.querySelector('select');
+const title = document.querySelector('title');
+const allLang = ['en', 'ru'];
+
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+
+    title.innerHTML = langArr['unit'][hash];
+
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+
+changeLanguage();
---
+++ src/js/lang.js
@@ -0,0 +1,116 @@
+const langArr = {
+    "unit": {
+        "ru": "Егор Василевский - веб-разработчик",
+        "en": "Egor Vasilevskiy - web developer"
+    },
+//Навигация
+    "home": {
+        "ru": "Главная",
+        "en": "Home"
+    },
+
+    "about": {
+        "ru": "Обо мне",
+        "en": "About me"
+    },
+
+    "skills": {
+        "ru": "Навыки",
+        "en": "Skills"
+    },
+
+    "portfolio": {
+        "ru": "Портфолио",
+        "en": "Portfolio"
+    },
+
+    "contacts": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    },
+//Интро
+    "logo": {
+        "ru": "Егор <br> Василевский",
+        "en": "Egor <br> Vasilevskiy"
+    },
+
+    "info": {
+        "ru": "Веб-разработчик <br> 21 год, Минск",
+        "en": "Web developer <br> 21 years old, Minsk"
+    },
+//Обо мне
+    "sectionAbout": {
+        "ru": "Обо мне",
+        "en": "About me"
+    },
+
+    "aboutMe1": {
+        "ru": "Привет, я Егор - веб-разработчик из Минска. Интересуюсь разработкой и всем, что с этим связано.",
+        "en": "Hi, I'm Egor – Web developer from Minsk. I am interested in development and everything related to it."
+    },
+
+    "aboutMe2": {
+        "ru": "Я начинающий разработчик с желанием узнавать все новое, что связано с моими технологиями.",
+        "en": "I am a beginner developer with a desire to learn everything new related to my technologies"
+    },
+
+    "aboutMe3": {
+        "ru": "Готов реализовывать отличные проекты с замечательными людьми.",
+        "en": "Ready to implement excellent projects with wonderful people."
+    },
+//Скилы
+    "sectionSkills": {
+        "ru": "Навыки",
+        "en": "Skills"
+    },
+
+    "skillsCopy": {
+        "ru": "Я работаю со следующими технологиями",
+        "en": "I work with such technologies :"
+    },
+//Портфолио
+    "sectionPortfolio": {
+        "ru": "Портфолио",
+        "en": "Portfolio"
+    },
+
+    "traveland": {
+        "ru": "Traveland - <br> Сайт для туристической компании",
+        "en": "Traveland - <br> Website for a travel company"
+    },
+
+    "websiteit": {
+        "ru": "IT Company - <br> Сайт для IT компании",
+        "en": "IT Company - <br> Website for a IT Company"
+    },
+
+    "cosmetic": {
+        "ru": "AgavaCosmet - <br> Сайт для мастера красоты <br> (В разработке)",
+        "en": "AgavaCosmet - <br> Site for a master beautician <br> (In developing)"
+    },
+
+    "calculator": {
+        "ru": "Калькулятор -<br> (В разработке)",
+        "en": "Calculator -<br> (In developing)"
+    },
+//Футер
+    "contactsFooter": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    },
+
+    "footerCopy": {
+        "ru": "Хотите узнать больше или просто поболтать? <br> Добро пожаловать!",
+        "en": "Want to know more or just chat? <br> You are welcome!"
+    },
+
+    "message": {
+        "ru": "Отправить сообщение",
+        "en": "Send message"
+    },
+
+    "footerSubtitle": {
+        "ru": "Лайкни меня на <br> LinkedIn, Instagram, Behance",
+        "en": " Like me on <br> LinkedIn, Instagram, Behance"
+    },
+}
